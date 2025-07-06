2dada13d2764f5fb07a175e80896e9be1cb84db5
CSHitt <turtleproductions4488@gmail.com>
2025-07-06 15:35:58 -0400

    Fix previous commits
    I accidentally put the files in the wrong place. Oops.

--- root/srt/Main.java
+++ root/srt/Main.java
@@ -8,7 +8,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640, 480);
 
-        RichContainer[] ContainerList = new RichContainer[5];
+        RichContainer[] ContainerList = new RichContainer[6];
         RichContainer template = new RichContainer();
         template.AddElement(1); // The type should be 0, the text should be "Enter some text into this box"
         template.AddElement(2);
@@ -16,24 +16,14 @@ public class Main {
         for (int i = 0; i < 5; i++){
             ContainerList[i] = template;
         }
-        // Load your image
-        ImageIcon icon = new ImageIcon("jockey.jpg"); // Or JPG, GIF, etc.
+        ContainerList[5] = new RichContainer(); // Add the image in its own container
+        ContainerList[5].AddElement(3, "jockey.jpg"); // The image, specifically inside the jar
         
-        // Create a label with the image
-        JLabel label = new JLabel(icon);
-        label.setBounds(200, 200, 100, 100);
-        // Create a label
+        // Add the Containers from ContainerList
         RichUtilities.AddRichContainersToJFrameSerially(frame, ContainerList, 0, 0);
-        // Create a button
-        JButton button = new JButton("Click Me!");
-        button.setBounds(240, 400, 100, 25);
-
-        // Add a listener to the button
 
         // Create a layout and add components
-        frame.setLayout(null);
-        frame.add(label);
-        frame.add(button);
+        frame.setLayout(null); // No organization
 
         // Show the frame
         frame.setVisible(true);
--- root/srt/RichContainer.java
+++ root/srt/RichContainer.java
@@ -2,12 +2,15 @@ package srt;
 import java.awt.*;
 import java.awt.image.BufferedImage; // For BufferedImage type
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList; // For ImageIcon type
 import java.util.List; // For File type
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.ImageIcon;
 
 public class RichContainer {
-    public RichContainer(){ // p for Position, w for Width
+    public RichContainer(){ // Will need to add stuff here later, like RichContainer ID
     }
     public void AddElement(){
         ElementArray.add(new Element()); // Add element. This function exists as a layer of abstraction so I don't have to write
@@ -20,6 +23,9 @@ public class RichContainer {
     }
     public void AddElement(int ElementType, File FileLocation){
         ElementArray.add(new Element(ElementType, FileLocation)); // Add element with ElementType
+    }
+    public void AddElement(int ElementType, String PathInsideJar){
+        ElementArray.add(new Element(ElementType, PathInsideJar)); // Add element with ElementType
     }
     public static void AddElementToJFrame(Element element, JFrame frame){ // Downside of using this function is that you cannot access the element in the JFrame once it
         frame.add(CreateJLabelFromElement(element));               // has been added
@@ -41,53 +47,73 @@ public class RichContainer {
     public Element GetElement(int Index){ // Get data of specific element. Like AddElement, it exists as a layer of abstraction so I don't have to write as much code
         return ElementArray.get(Index);
     }
-    public static int GetStandardFontHeight(Element element){ // Get font heigh for text without font modification
+    public static int GetElementHeight(Element element){ // Get font heigh for text without font modification
         String defaultFontName = UIManager.getFont("Label.font").getFontName(); // get standard font data
         int defaultFontSize = UIManager.getFont("Label.font").getSize();
+        int height = 0;
         Font selectedFont;
+        FontMetrics metrics;
         switch (element.Type) {
             case 0: // Plaintext font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize)); // Create new font with correct size
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                height = metrics.getHeight(); // Get height
                 break;
             case 1: // Title font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 2));
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                height = metrics.getHeight(); // Get height
                 break;
             case 2: // Subtitle font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 1.5f));
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                height = metrics.getHeight(); // Get height
+                break;
+            case 3:
+                ImageIcon temp = (ImageIcon)element.Content;
+                height = temp.getIconHeight();
                 break;
             default:
-                throw new IllegalArgumentException("The element provided does not match one of the accepted types (0-2)"); // throw error if it is not the right type
+                throw new IllegalArgumentException("The element provided does not match one of the accepted types (0-3)"); // throw error if it is not the right type
         }
-        FontMetrics metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
-        int height = metrics.getHeight(); // Get height
         return height;
     }
-    public static int GetElementTextWidth(Element element){ // Similar execution to GetStandardFontHeight
+    public static int GetElementWidth(Element element){ // Similar execution to GetElementHeight
         String defaultFontName = UIManager.getFont("Label.font").getFontName(); 
         int defaultFontSize = UIManager.getFont("Label.font").getSize();
         Font selectedFont;
+        FontMetrics metrics;
+        int width;
         switch (element.Type) {
             case 0: // Plaintext font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize));
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                width = metrics.stringWidth(element.Content.toString()); // get width instead
                 break;
             case 1: // Title font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 2));
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                width = metrics.stringWidth(element.Content.toString()); // get width instead
                 break;
             case 2: // Subtitle font size
                 selectedFont = new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 1.5f));
+                metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
+                width = metrics.stringWidth(element.Content.toString()); // get width instead
+                break;
+            case 3:
+                ImageIcon temp = (ImageIcon)element.Content;
+                width = temp.getIconWidth();
                 break;
             default:
                 throw new IllegalArgumentException("The element provided does not match one of the accepted types (0-2)");
         }
-        FontMetrics metrics = Toolkit.getDefaultToolkit().getFontMetrics(selectedFont); // Note: .getFontMetrics is deprecated
-        int width = metrics.stringWidth(element.Content.toString()); // get width instead
         return width;
     }
     public static int GetMaximumRichContainerHeight(RichContainer container){
         int heightRegister = 0; // Keeps store of the current max height
         for (int i = 0; i < container.ElementArray.size(); i++){ // iterates through all elements of the container
-            if (GetStandardFontHeight(container.ElementArray.get(i)) > heightRegister) { // if an element is taller than the current recorded height...
-                heightRegister = GetStandardFontHeight(container.ElementArray.get(i)); // its height becomes the new max height
+            if (GetElementHeight(container.ElementArray.get(i)) > heightRegister) { // if an element is taller than the current recorded height...
+                heightRegister = GetElementHeight(container.ElementArray.get(i)); // its height becomes the new max height
             }
         }
         return heightRegister;
@@ -113,21 +139,27 @@ public class RichContainer {
         int defaultFontSize = UIManager.getFont("Label.font").getSize();
         switch(element.Type){
             case 0:
-                label = new JLabel(element.Content.toString()); // Plain, default text
+                label = new JLabel(element.Content.toString()); // Get text from the element
+                label.setBounds(0, 0, GetElementWidth(element), GetElementHeight(element)); // Set label size
                 break;
             case 1:
                 label = new JLabel(element.Content.toString());
                 label.setFont(new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 2))); // Font size is twice as large
+                label.setBounds(0, 0, GetElementWidth(element), GetElementHeight(element));
                 break;
             case 2: 
                 label = new JLabel(element.Content.toString());
                 label.setFont(new Font(defaultFontName, Font.PLAIN, (int)(defaultFontSize * 1.5f))); // Font size is 1.5x as large
+                label.setBounds(0, 0, GetElementWidth(element), GetElementHeight(element));
+                break;
+            case 3:
+                label = new JLabel((ImageIcon)(element.Content)); // Create label from specified image in element
+                label.setBounds(0, 0, GetElementWidth(element), GetElementHeight(element));
                 break;
             // Implement 3,4 later
             default:
                 throw new IllegalArgumentException("The element that was inputted is corrupted");
         }
-        label.setBounds(0, 0, GetElementTextWidth(element), GetStandardFontHeight(element));
         return label;
     }
     public class Element { 
@@ -169,13 +201,31 @@ public class RichContainer {
         }
 
         Element(int ElementType, File FileLocation){ // Only for Type 3. CanvasType is the same as imageType
-            if (ElementType != 3){ // This constructor is only for Type 4
+            if (ElementType != 3){ // This constructor is only for Type 3
                 throw new IllegalArgumentException("Only an element of Type 3 (Static Canvas) may take a File Location");
             } else if (FileLocation.canRead() != true){
                 throw new IllegalArgumentException("The program is unable to access the file specified");
             } else {
                 Type = ElementType;
                 Content = new ImageIcon(FileLocation.getPath());
+            }
+            
+        }
+
+        Element(int ElementType, String PathInsideJar){ // For type 3, but for files in the .jar. PathInsideJar is relative to the RichContainer class
+            if (ElementType != 3){ // This constructor is only for Type 4
+                throw new IllegalArgumentException("Only an element of Type 3 (Static Canvas) may take a File Location");
+            } else {
+                try {
+                    InputStream input = RichContainer.class.getResourceAsStream(PathInsideJar); // Get the InputStream to the image
+                    BufferedImage img = ImageIO.read(input); // Get the Image type from the image
+                    Content = new ImageIcon(img); // Set content to be the ImageIcon of Image
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("The program is unable to access the file specified"); // Happens if the file is not accessable;
+                                                                                                              // Also solves checked exceptions
+                }
+                Type = ElementType; // element variable Type
+                
             }
             
         }
